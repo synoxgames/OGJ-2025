672a29305fe3f4b3255ea91c56d669cf11b9c4b3
BrooklynT101 <brooklynigtaylor@gmail.com>
2025-07-25 22:52:29 +1200

    Added music control and goofy music

--- Assets/Scripts/SettingMenu.cs
+++ Assets/Scripts/SettingMenu.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingMenu : MonoBehaviour
 {
+
+    [SerializeField] public AudioMixer audioMixer;
+    [SerializeField] public Slider musicSlider;
+    [SerializeField] public Slider sfxSlider;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
     }
 }
