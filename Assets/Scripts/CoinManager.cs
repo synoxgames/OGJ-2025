2d67281602cc982405ad78b6258dd19477cf4e77
Nkanyiso <ndlnk218@student.otago.ac.nz>
2025-07-26 16:30:56 +1200

    Implmented Scene transitions

--- Assets/Scripts/CoinManager.cs
+++ Assets/Scripts/CoinManager.cs
@@ -1,35 +1,42 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using System.Globalization;
-//using System.Runtime.CompilerServices;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using UnityEngine;
 
-///* Currently : 
-//    CoinManager is initialized on gameLoad, 
-//    CoinManager keeps track of the coin count, but it is not persistent across game sessions.
-    
-//    Future improvements:
-//        - Make coin count persistent across game sessions using playerrefs etc.
-//*/
+/* Currently : 
+    CoinManager is initialized on gameLoad, 
+    CoinManager keeps track of the coin count, but it is not persistent across game sessions.
+
+    Future improvements:
+        - Make coin count persistent across game sessions using playerrefs etc.
+*/
 
 
-//public class CoinManager : MonoBehaviour
-//    public static int coinCount;
-//{
-    
-//    //public static Text coinsText; // Static variable to hold the UI text to make it easier to update, if needed
-
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        // Initialize coin count if needed
-//        coinCount = 100;
-//    }
-    
-
-//    // Update is called once a game is completed.
-//    void setCoins(int value)
-//    {
-//        cointCount += value;
-//    }
-//}
+public class CoinManager : MonoBehaviour
+{
+    public static CoinManager Instance { get; private set; }
+    private int coinCount = 100; // Intial coin count set to 100
+    private void Awake() 
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Ensure this instance persists across scenes
+        }
+        else
+        {
+            Destroy(gameObject); // Ensure only one instance exists
+        }
+    }
+    public void AddCoins(int amount)
+    {
+        coinCount += amount;
+        Debug.Log($"Coins added: {amount}. Total coins: {coinCount}");
+    }
+    public int GetCoinCount()
+    {
+        return coinCount;
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public GameObject settingsWindow;
+
+    public void startGame()
     {
-        
+        Debug.Log("Start game clicked");
+        //Load the Meuseum cut scene timed
+        SceneManager.LoadScene("MuseumExterior");
+        new WaitForSeconds(1f);
+        Debug.Log("Waited 1 sec");
+
+
+        //SceneManager.LoadScene("Level1");
+        //SceneManager.LoadSceneAsync("NextSvener", LoadSceneMode.Additive); // add it on top of the scene, "pause"
+        //SceneManager.UnloadScene("NextSvener");
+
+        // For other scenes.
     }
 
-    // Update is called once per frame
-    void Update()
+    //Function performs the features of pausing the game, it may pause the state in the game.
+    public void pauseGame()
     {
-        
+        SceneManager.LoadSceneAsync("PauseMenuSettings", LoadSceneMode.Additive); // add it on top of the scene, "pause"
+        Time.timeScale = 0; // pause time in the game.
     }
+    public void unPauseGame()
+    {
+        SceneManager.UnloadSceneAsync("PauseMenuSettings");
+        Time.timeScale = 1; // restore state/time.
+    } 
+
 }
